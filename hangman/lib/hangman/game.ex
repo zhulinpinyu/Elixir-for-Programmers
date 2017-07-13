1ece3f0933d00ef171c354941ff55d5266b5d5f5
zhulinpinyu <mulixiang199016@gmail.com>
2017-07-14 07:22:13 +0800

    Score a Good move

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -6,10 +6,14 @@ defmodule Hangman.Game do
     used: MapSet.new()
   )
 
-  def new_game() do
+  def new_game(word) do
     %Hangman.Game{
-      letters: Dictionary.random_word |> String.codepoints
+      letters: word |> String.codepoints
     }
+  end
+
+  def new_game() do
+    new_game(Dictionary.random_word)
   end
 
   def make_move(%{game_state: state} = game, _) when state in [:won, :lost] do
@@ -27,7 +31,22 @@ defmodule Hangman.Game do
 
   def accept_move(game, guess, false) do
     Map.put(game, :used, MapSet.put(game.used, guess))
+    |> score_guess(Enum.member?(game.letters, guess))
   end
+
+  def score_guess(game, true) do
+    new_state = MapSet.new(game.letters)
+    |> MapSet.subset?(game.used)
+    |> maybe_won()
+    Map.put(game, :game_state, new_state)
+  end
+
+  def score_guess(game, _) do
+    game
+  end
+
+  def maybe_won(true), do: :won
+  def maybe_won(_), do: :good_guess
 
   defp tally(_game) do
     123
--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -34,4 +34,33 @@ defmodule GameTest do
     {game, _} = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
+
+  test "a good guess is recognized" do
+    game = Game.new_game("williman")
+    {game, _} = Game.make_move(game, "m")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a guessed word is a won game" do
+    game = Game.new_game("williman")
+    {game, _} = Game.make_move(game, "w")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, _} = Game.make_move(game, "i")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, _} = Game.make_move(game, "l")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, _} = Game.make_move(game, "m")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, _} = Game.make_move(game, "a")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+    {game, _} = Game.make_move(game, "n")
+    assert game.game_state == :won
+    assert game.turns_left == 7
+  end
 end
