710eec03644a63fbee57798de8a5fc9c3dd3a1bd
zhulinpinyu <mulixiang199016@gmail.com>
2017-07-18 22:47:21 +0800

    send receive 练习

--- basic_process/code/procs.exs
+++ basic_process/code/procs.exs
@@ -1,9 +1,9 @@
 defmodule Procs do
-  def greeter() do
+  def greeter(what_to_say) do
     receive do
       msg ->
-        IO.puts "Hi #{IO.inspect msg}"
+        IO.puts "#{what_to_say}: #{inspect msg}"
     end
-    greeter()
+    greeter(what_to_say)
   end
 end
