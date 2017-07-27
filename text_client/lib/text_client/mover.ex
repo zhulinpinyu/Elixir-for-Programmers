db331472b41f73ca0e68d99bfb2b2800faf2bb7c
zhulinpinyu <mulixiang199016@gmail.com>
2017-07-27 14:37:35 +0800

    A Quick API Change

--- text_client/lib/text_client/mover.ex
+++ text_client/lib/text_client/mover.ex
@@ -3,7 +3,7 @@ defmodule TextClient.Mover do
   alias TextClient.State
 
   def make_move(state) do
-    {gs, tally} = Hangman.make_move(state.game_service, state.guess)
-    %State{state| game_service: gs, tally: tally}
+    tally = Hangman.make_move(state.game_service, state.guess)
+    %State{state| tally: tally}
   end
 end
