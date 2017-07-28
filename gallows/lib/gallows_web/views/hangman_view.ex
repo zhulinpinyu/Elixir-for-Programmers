b876d4cc47de285c0d93e9145319b625716d490f
zhulinpinyu <mulixiang199016@gmail.com>
2017-07-28 12:00:16 +0800

    Wrapping Up: Adding Graphics

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -10,4 +10,10 @@ defmodule GallowsWeb.HangmanView do
   def new_game_button(conn) do
     button("New Game", class: "btn btn-default", to: hangman_path(conn, :create_game))
   end
+
+  def turn(left, target) when target >= left do
+    "opacity: 1"
+  end
+
+  def turn(left, target), do: "opacity: 0.1"
 end
